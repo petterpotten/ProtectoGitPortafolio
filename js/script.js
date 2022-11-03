f109bbc4eef8e2307593b34df524bc4312904f54
petterpotten <ppotenciano@farmacias.local>
2022-11-03 16:28:37 -0700

    Se realiza Click Pup Up en todos los qr fatantes

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ document.querySelector('.regresa_menu_container')
 var modal = document.getElementById('popUp');
 
 var imgpersona = document.getElementById('imgpersona');
-var imgEnModalPersona = document.getElementById("imgEnModalPersona");
+var imgEnModalPersona = document.getElementById("imgEnModal");
 var descripcion = document.getElementById("descripcion");
 
 imgpersona.onclick = function () {
@@ -44,11 +44,53 @@ span.onclick = function () {
 // Zoom en qr cartoink al dar click
 var modal = document.getElementById('popUp');
 
-var imgpersona = document.getElementById('qrcarttoink');
-var imgEnModalPersona = document.getElementById("imgEnModalPersona");
+var qrcarttoink = document.getElementById('qrcarttoink');
+var imgEnModalPersona = document.getElementById("imgEnModal");
 var descripcion = document.getElementById("descripcion");
 
 qrcarttoink.onclick = function () {
+	modal.style.display = "block";
+	imgEnModalPersona.src = this.src;
+	descripcion.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+	modal.style.display = "none";
+}
+
+
+
+// Zoom en qr omerta al dar click
+var modal = document.getElementById('popUp');
+
+var qromerta = document.getElementById('qromerta');
+var imgEnModalPersona = document.getElementById("imgEnModal");
+var descripcion = document.getElementById("descripcion");
+
+qromerta.onclick = function () {
+	modal.style.display = "block";
+	imgEnModalPersona.src = this.src;
+	descripcion.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+	modal.style.display = "none";
+}
+
+
+
+// Zoom en qr makeorganic al dar click
+var modal = document.getElementById('popUp');
+
+var qrmakeorganic = document.getElementById('qrmakeorganic');
+var imgEnModalPersona = document.getElementById("imgEnModal");
+var descripcion = document.getElementById("descripcion");
+
+qrmakeorganic.onclick = function () {
 	modal.style.display = "block";
 	imgEnModalPersona.src = this.src;
 	descripcion.innerHTML = this.alt;
